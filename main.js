5661c6539769d2978bb9f00bc5a27021ea326869
Alessandro Muratore <muratore@gmail.com>
2022-12-24 18:59:03 -0300

    aplicando sass no projto

--- main.js
+++ main.js
@@ -4,21 +4,28 @@ const validarBtn = document.querySelector("#checkEntrance")
 
 validarBtn.addEventListener('click', (e) =>{
   e.preventDefault()
-  const name = document.querySelector('#name').value;
-  const age = document.querySelector('#age').value;
+  const name = String(document.querySelector('#name').value);
+  const age = Number(document.querySelector('#age').value);
   checkAccess(age, name)
 })
 // age, name
 function checkAccess(age, name) {
+  
   if (age === "" || name === "") {
-    showTxt.innerText=`Preencha os campos de idade e nome`;
+    showTxt.innerText=`💥 Preencha os campos de idade e nome`;
     showTxt.style.color='tomato'
   } else if (age >= 60) {
-    showTxt.innerText=`${name}, sua entrada gratuita`;
+    showTxt.innerText=`${name}, sua entrada gratuita 🍹 `;
+    showTxt.style.color='black'
   } else if(age >= 18 && age < 60){
-    showTxt.innerText= `Pode entrar ${name}. Enjoy it!!!! 🍹`;
-  } else {
+    showTxt.innerText= `Pode entrar ${name}. Enjoy it!!!! 🍹 🍾`;
+    showTxt.style.color='green';
+  } else if(!age || !name){
+    showTxt.innerText="Apenas caractéres para nome e apenas dígitos para idade";
+    }
+  else {
     showTxt.innerText=`${name} você é menor de idade`;
+    showTxt.style.color='tomato'
   }
 }
 
