e6742cc9179453031bb6038cc471f11df540c3b5
Alessandro Muratore <muratore@gmail.com>
2022-12-26 10:50:47 -0300

    Correção da lógica do Código

--- main.js
+++ main.js
@@ -2,34 +2,35 @@
 const showTxt = document.querySelector('.showTxt h2');
 const validarBtn = document.querySelector("#checkEntrance")
 
+
 validarBtn.addEventListener('click', (e) =>{
   e.preventDefault()
   const name = String(document.querySelector('#name').value);
   const age = Number(document.querySelector('#age').value);
-  checkAccess(age, name)
-})
-// age, name
-function checkAccess(age, name) {
   
   if (age === "" || name === "") {
     showTxt.innerText=`💥 Preencha os campos de idade e nome`;
     showTxt.style.color='tomato'
-  } else if (age >= 60) {
-    showTxt.innerText=`${name}, sua entrada gratuita 🍹 `;
-    showTxt.style.color='black'
+    console.log('age === "" || name === "');
+  }else if(!age || !name){
+    showTxt.innerText="Apenas caractéres para nome e dígitos para idade";
+    showTxt.style.color='tomato'
+    console.log('age || name');
   } else if(age >= 18 && age < 60){
-    showTxt.innerText= `Pode entrar ${name}. Enjoy it!!!! 🍹 🍾`;
+    showTxt.innerText= `Pode entrar ${name}. Enjoy it!!!!  🍹 🍾`;
     showTxt.style.color='green';
-  } else if(!age || !name){
-    showTxt.innerText="Apenas caractéres para nome e apenas dígitos para idade";
-    }
-  else {
+    console.log('age >= 18 && age < 60');
+  }else if (age >= 60) {
+    showTxt.innerText=`${name}, sua entrada é gratuita 🍹 🍾 🎉`;
+    showTxt.style.color='green'
+  } else {
     showTxt.innerText=`${name} você é menor de idade`;
     showTxt.style.color='tomato'
   }
-}
+})
 
-// limpar mensagem
+
+// limpar mensagem showTxt
 const reset = document.querySelector('#reset').addEventListener('click', ()=>{
   showTxt.innerText="";
 })
